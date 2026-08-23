370eecab15baf13d9c4c46e63cef36b9d5f95068
Nihad <47224765+lin0les@users.noreply.github.com>
2026-08-23 21:49:22 +0400

    convert printd to custom itoa
    Now gets 2 args, char *  and number to convert.

--- my_itoa.c
+++ my_itoa.c
@@ -1,12 +1,16 @@
-void printd(int n)
-{
-    if (n < 0) {
-        putchar('-');
+#include <stdio.h>
+
+void printd(int n, char s[]){
+    static int i = 0;
+    if(n < 0){
+        s[i++] = '-';
         n = -n;
     }
 
-    if (n / 10)
-        printd(n / 10);
+    if(n / 10){
+        printd(n / 10, s);
+    }
 
-    putchar(n % 10 + '0');
+    s[i++] = n % 10 + '0';
+    s[i] = '\0';
 }
