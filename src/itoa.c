52d2003c8d0615a9289e63e5f004035227cd572c
Nihad <47224765+lin0les@users.noreply.github.com>
2026-08-30 13:30:34 +0400

    Update itoa.c pointer version

--- src/itoa.c
+++ src/itoa.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 
-void itoa(int n, char s[]){
-    static int i = 0;
+// void itoa(int n, char s[]){
+//     static int i = 0;
+//     if(n < 0){
+//         s[i++] = '-';
+//         n = -n;
+//     }
+//
+//     if(n / 10){
+//         itoa(n / 10, s);
+//     }
+//
+//     s[i++] = n % 10 + '0';
+//     s[i] = '\0';
+// }
+
+char *itoa(int n, char *s){
     if(n < 0){
-        s[i++] = '-';
+        *s++ = '-';
         n = -n;
     }
 
     if(n / 10){
-        itoa(n / 10, s);
+       s = itoa(n / 10, s);
     }
 
-    s[i++] = n % 10 + '0';
-    s[i] = '\0';
+    *s++ = n % 10 + '0';
+    *s = '\0';
+    return s;
 }
