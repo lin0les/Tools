27b7ea75260b7bda6023ff7a22d0e286cb29c86e
lin0les <777nihad@gmail.com>
2026-09-17 18:40:39 +0400

    Update leapyear.c: adding month_name()

--- leapyear/leapyear.c
+++ leapyear/leapyear.c
@@ -35,3 +35,14 @@ int month_day(int year, int yearday, int *pmonth, int *pday){
     *pmonth=i;
     *pday=yearday;
 }
+
+char *month_name(int n){
+    static char *name[]={
+        "Illegal Month", "January", "Febuary", "March", 
+        "April", "May", "June",
+        "July", "August", "September",
+        "October", "November", "December"
+    };
+
+    return (n < 1 || n > 12) ? name[0] : name[n];
+}
--- leapyear/main.c
+++ leapyear/main.c
@@ -2,16 +2,21 @@
 
 int day_of_year(int year, int month, int day);
 int month_day(int year, int yearday, int *pmonth, int *pday);
+char *month_name(int n);
 
 int main(int argc, char *argv[]){
 
     int yearday, year, month, day;
-    year=2002;
-    yearday=370;
-    month=11;
-    day=22;
+    year=1996;
+    month=9;
+    day=28;
+    yearday=326;
     
     int ret;
+
+    printf("Year = %d\n", year);
+    printf("Month = %d\n", month);
+    printf("Day = %d\n", day);
 
     ret=day_of_year(year, month, day);
     if(ret == -1)
@@ -21,13 +26,14 @@ int main(int argc, char *argv[]){
     else
         printf("Day of year => %d\n", ret);
 
+    printf("\nYearday = %d\n", yearday);
     ret = month_day(year, yearday, &month, &day);
     if(ret == -1)
         printf("Invalid year\n");
     else if(ret == -2)
         printf("Invalid yearday\n");
     else
-        printf("Day of year =>=> month => %d; day => %d\n", month, day);
+        printf("Day of year => month => %s(%d); day => %d\n", month_name(month), month, day);
 
     return 0;
 }
