756423556af52d49d41c083e605db8aa067965b7
lin0les <777nihad@gmail.com>
2026-09-21 17:41:25 +0000

    modified grep

--- grep/grep.c
+++ grep/grep.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-#define MAXLINE 4
+#define MAXLINE 1000
 
 int mygetline(char *line, int max){
     int i,c;
@@ -17,16 +17,38 @@ int mygetline(char *line, int max){
 int main(int argc, char *argv[]){
 
     char line[MAXLINE];
-    int found=0;
+    long lineno=0;
+    int c, except=0, number=0, found=0;
 
-    if(argc != 2)
-        printf("Usage: find patter\n");
+    while(--argc > 0 && (*++argv)[0] == '-')
+        while(c=*++argv[0])
+            switch(c){
+                case 'x':
+                    except=1;
+                    break;
+                case 'n':
+                    number=1;
+                    break;
+                default:
+                    printf("find: illegal option %c\n", c);
+                    argc=0;
+                    found=-1;
+                    break;
+            }
+
+
+    if(argc != 1)
+        printf("Usage: find -x -n patter\n");
     else
-        while(mygetline(line, MAXLINE) > 0)
-            if(strstr(line, argv[1]) != NULL){
+        while(mygetline(line, MAXLINE) > 0){
+            lineno++;
+            if((strstr(line, *argv) != NULL) != except){
+                if(number)
+                    printf("%ld: ", lineno);
                 printf("%s\n", line);
                 found++;
             }
+        }
     return found;
 }
 
